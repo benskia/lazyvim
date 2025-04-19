9d6b450a0326f6c4261d30e341d9dfcda11c8918
distrollo <AbrahamBenski@proton.me>
2025-04-19 18:20:45 -0500

    disable flash; add opts to surrounds and splitjoin

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -191,6 +191,8 @@ return {
         "shellcheck",
         "shfmt",
         "flake8",
+        "gopls",
+        "pgformatter",
       },
     },
   },
---
+++ lua/plugins/mini.lua
@@ -0,0 +1,4 @@
+return {
+  { "echasnovski/mini.splitjoin", version = false, opts = {} },
+  { "echasnovski/mini.surround", version = false, opts = {} },
+}
