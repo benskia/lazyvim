aef0cef2127809562d36a96746acea4963cc78b2
distrollo <AbrahamBenski@proton.me>
2025-05-04 16:09:40 -0500

    disable lspconfig inlay hints

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -63,15 +63,15 @@ return {
     },
   },
 
-  -- add pyright to lspconfig
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
       ---@type lspconfig.options
+      inlay_hints = { enabled = false },
       servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
+        rust_analyzer = {},
       },
     },
   },
